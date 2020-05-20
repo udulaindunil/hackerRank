7982a7cd2c524622b2c5ceb21ce6e40ccf3d7533
udulaindunil <udulaindunil@gmail.com>
2020-05-21 02:31:30 +0530

    Mini-Max Sum

--- src/hackerRank/Solution.java
+++ src/hackerRank/Solution.java
@@ -10,30 +10,47 @@ import java.util.regex.*;
 
 public class Solution {
 
-    // Complete the staircase function below.
-    static void staircase(int n) {
- 
-    	for (int i = 0; i < n; i++) {
-    		int p = i+1;
-			for (int j = 0; j <n-p; j++) {
-				System.out.print(' ');
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+    	
+    	Arrays.sort(arr);
+    	Integer x=0,y=0;
+
+         if(arr.length<=4) {
+        	 Integer r=0;
+        	 for (int i = 0; i < arr.length; i++) {
+				r=r+arr[i];
 			}
-			
-			for (int j = 0; j <p; j++) {
-				System.out.print('#');
-			}
-			System.out.println(' ');
+        	 System.out.printf("%d %d ",r,r);
+        	 return ;
+         }
+         
+    	for (int i = 0; i < 4; i++) {
+			x=x+arr[i];
 		}
+    	for (int i = arr.length-1; i > arr.length-5 ; i--) {
+			y=y+arr[i];
+		}
+    	
+    	System.out.printf("%d %d ",x,y);
+    	
 
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = scanner.nextInt();
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        staircase(n);
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
 
         scanner.close();
     }
